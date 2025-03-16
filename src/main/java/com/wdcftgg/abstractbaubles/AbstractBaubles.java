0d07987656c3ca8934840e91f085e815eb2216f6
xuanyu tang <105061290+ukmojb@users.noreply.github.com>
2025-03-16 14:45:06 +0800

    fix mixin

--- src/main/java/com/wdcftgg/abstractbaubles/AbstractBaubles.java
+++ src/main/java/com/wdcftgg/abstractbaubles/AbstractBaubles.java
@@ -12,12 +12,12 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = AbstractBaubles.MODID, name = AbstractBaubles.NAME, version = AbstractBaubles.VERSION, dependencies="required-after:baubles;after:botania,classicbar")
+@Mod(modid = AbstractBaubles.MODID, name = AbstractBaubles.NAME, version = AbstractBaubles.VERSION, dependencies="required-after:baubles")
 public class AbstractBaubles {
 
     public static final String MODID = "abstractbaubles";
     public static final String NAME = "AbstractBaubles";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4";
     public static Logger logger;
 
     public static final String CLIENT_PROXY_CLASS = "com.wdcftgg.abstractbaubles.proxy.ClientProxy";
